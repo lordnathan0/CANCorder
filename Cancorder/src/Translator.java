5435dd34a9d57756bbef2818d84407b981801a59
Sean <sean@ubuntu.(none)>
2013-11-27 16:43:18 -0800

    Translator fixes

--- Cancorder/src/Translator.java
+++ Cancorder/src/Translator.java
@@ -26,20 +26,21 @@ public class Translator {
 			ArrayList<Signal<String, Integer, Integer, Integer, String>> dbfSignals = dbfMap.dbfMap.get(messageID);
 			for (Signal<String, Integer, Integer, Integer, String> signal : dbfSignals) {
 				
+				int startBit = signal.startBit;
 				//Create a ByteBuffer that holds 8 bytes (entire message)
 				ByteBuffer buffer = ByteBuffer.allocate(8);
 				//Get data order and change ByteBuffer to BIG_ENDIAN OR LITTLE_ENDIAN ** BIG_ENDIAN BY DEFAULT
 				if(signal.byteOrder.equals(0)) {
 					buffer.order(ByteOrder.BIG_ENDIAN);
+					startBit = 64 - (8*(signal.startBit/8)) - ((8-((signal.startBit+1)%8))%8) - signal.length;
 				}
 				else {
 					buffer.order(ByteOrder.LITTLE_ENDIAN);
 				}
 				buffer.put(frameData);
 				buffer.position(0);
-				//Get most recent bike data. MAY BE MORE EFFICIENT YET CORRECT TO THROW OUTSIDE THE LOOP
 				long bitmask = 0;
-				long power = (signal.startBit);
+				long power = (startBit);
 				for(int i = 0; i < signal.length.intValue(); i ++) {
 					bitmask = (bitmask | (1L << power));
 					power ++;
@@ -47,66 +48,48 @@ public class Translator {
 				
 				long bitData = 0;
 				long adjustment = 0;
-				
 				bitData = buffer.getLong();
 
 				byte[] recompiledData = new byte[buffer.limit()];
 				bitData = bitData & bitmask;
-				if(signal.byteOrder.equals(0)) { // If signal big endian
-					bitData = bitData >>> signal.startBit; //Changed to >>> recently from >>
-					long negativeThreshold = 2 << (signal.length-2);
-					if(signal.dataType.equals("+") && (negativeThreshold <= bitData)) {
-						for(int i = signal.length; i < 64; i++) {
-							adjustment = (adjustment | (1L << i));
-						}
-						bitData = bitData | adjustment;
+				bitData = bitData >>> startBit;
+				
+				long negativeThreshold = 2 << (signal.length-2);
+				if(signal.dataType.equals("-") && (negativeThreshold <= bitData)) {
+					for(int i = signal.length; i < 64; i++) {
+						adjustment = (adjustment | (1L << i));
+					}
+					bitData = bitData | adjustment;
+				}
+				
+				if(signal.byteOrder.equals(0)){
+					for(int i = 7, j = 0; i >= 0; i--, j++) {
+						recompiledData[i] = (byte)(bitData >> j*8);
 					}
 				}
 				else {
-					bitData = bitData << (long)(64 - (signal.startBit+signal.length));
-					if(signal.dataType.equals("+")) {
-						for(int i = 0; i < 64 - signal.length; i++) {
-							adjustment = (adjustment | (1L << i));
-						}
-						bitData = bitData | adjustment;
+					for(int i = 0, j = 0; i <= 7; i++, j++) {
+						recompiledData[i] = (byte)(bitData >> j*8);
 					}
 				}
 				
-				for(int i = 7, j = 0; i >= 0; i--, j++) {
-					recompiledData[i] = (byte)(bitData >> j*8);
-				}
+				
 				buffer.clear();
 				buffer = buffer.put(recompiledData);
 				buffer.position(0);
 				if(signal.dataType.equals("float")) {
-					buffer.order(ByteOrder.BIG_ENDIAN);
 					double data = buffer.getFloat();
 					data += buffer.getFloat();
 					bikeData.put(signal.signalID, data);
 				}
 				else if(signal.dataType.equals("-")) {
-					/*
-					if(signal.byteOrder.equals("0")) {
-						buffer.position(4);
-					}
-					else {
-						buffer.position(0);
-					}
-					*/
 					long data = buffer.getLong();
-					data = data << 64 - signal.length;
-					data = data >>> 64-signal.length; // Not needed??
 					bikeData.put(signal.signalID, (double)data);
 				}
 				else if(signal.dataType.equals("+")) {
-					/*if(signal.byteOrder.equals("0")) {
-						buffer.position(4);
-					}
-					else {
-						buffer.position(0);
-					}
-					*/
 					long data = buffer.getLong();
+					data = data << 64 - signal.length;
+					data = data >>> 64-signal.length; // Not needed??
 					bikeData.put(signal.signalID, (double)data);
 				}
 				else{
@@ -115,8 +98,6 @@ public class Translator {
 					bikeData.put(signal.signalID, data);
 				}
 			}
-			
-
 		}
 	}	
 }
